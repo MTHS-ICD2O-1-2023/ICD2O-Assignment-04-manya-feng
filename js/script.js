bd16342b95f693f14421dfab63dea65d105b9f82
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-01 01:35:49 +0000

    change js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,35 @@
 // This file contains the JS functions for index.html
 "use strict"
 
-function submit() {
-    const size = parseInt(document.getElementById("size").value)
+function calculate() {
+    let size = document.getElementById("size").value
+    let toppings = document.getElementById("toppings").value
+
+    let sizePrice = 0
+    let toppingsPrice = 0
+
+    if (size == "Large"){
+        sizePrice = 6.00
+    } else if (size == "Extra large") {
+        sizePrice = 11.00
+    } else {
+        document.getElementById("size").innerHTML = "Error"
+    }
+
+    if (toppings == "1 topping") {
+        toppingsPrice = 1.00
+    } else if (toppings == "2 toppings") {
+        toppingsPrice = 1.75
+    } else if (toppings == "3 toppings") {
+        toppingsPrice = 2.50
+    } else if (toppings == "3 toppings") {
+        toppingsPrice = 3.35
+    } else {
+        document.getElementById("toppings").innerHTML = "Error"
+    }
+
+    let totalPrice = ( sizePrice + toppingsPrice ) * 13% + ( sizePrice + toppingsPrice)
+
+    document.getElementById("total").innerHTML =
+    "Your total will be $" + totalPrice + "Enjoy your meal!"
 }
